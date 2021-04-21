cefd1039ef258e4cb3e99cf358262eb26d5a8f8d
Andy Nguyen <Ydna317@gmail.com>
2021-04-21 01:24:32 -0700

    Created schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,27 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE category (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10.00,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(30)
+);
+
+CREATE TABLE producttag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
+)
